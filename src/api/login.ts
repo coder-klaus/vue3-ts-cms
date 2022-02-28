1300e9a072d66c33687c2cc93707160864d9438f
klaus <klaus@klaus.com>
2022-02-28 15:08:02 +0800

    feat(loginstore): update on the use of pinia

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,8 +1,6 @@
 import api from '../utils/api'
 
-// Record<K, T> --- 生成一个新的对象类型，键的类型为K, 值的类型为T
-// Record<string, never> --- 等价于 空对象
-export function login<T, R>(account: T) {
+export function userLogin<T, R>(account: T) {
   return api.post<T, R>('/login', account)
 }
 
--- src/store/modules/login/index.ts
+++ src/store/modules/login/index.ts
@@ -1,66 +1,61 @@
 import { defineStore } from 'pinia'
 import Cookies from 'js-cookie'
+import { ref } from 'vue'
 import router from '/src/router/index'
-import { login, getUserInfo, getMenu } from '../../../api/login'
+import { userLogin, getUserInfo, getMenu } from '/src/api/login'
 import { IAccount, ILoginType, IUserInfo } from './types'
-import { IResponseType } from '../../../types'
+import { IResponseType } from '/src/types'
 
-interface IState {
-  token: string
-  userInfo: IUserInfo | null
-  menus: []
-}
+export default defineStore('loginStore', () => {
+  const tokenRef = ref('')
+  const userInfoRef = ref<IUserInfo | null>(null)
+  const menusRef = ref('[]')
 
-export default defineStore('loginStore', {
-  state: (): IState => ({
-    token: '',
-    userInfo: null,
-    menus: []
-  }),
+  async function fetchUserInfo(id: number) {
+    const { data } = await getUserInfo<IResponseType<IUserInfo>>(id)
+    userInfoRef.value = data
+    Cookies.set('userInfo', JSON.stringify(data))
+  }
 
-  actions: {
-    async login(account: IAccount) {
-      const { data } = await login<IAccount, IResponseType<ILoginType>>(account)
-      const { token, id } = data
+  async function fetchMenu(id: number) {
+    const { data } = await getMenu(id)
+    menusRef.value = data
+    Cookies.set('menus', JSON.stringify(data))
+    console.log(JSON.stringify(data))
+  }
 
-      this.$state.token = token
-      Cookies.set('token', token)
+  async function login(account: IAccount) {
+    const { data } = await userLogin<IAccount, IResponseType<ILoginType>>(account)
+    const { token, id } = data
 
-      this.fetchUserInfo(id)
-      this.fetchMenu(id)
+    tokenRef.value = token
+    Cookies.set('token', token)
 
-      router.push('/home')
-    },
+    fetchUserInfo(id)
+    fetchMenu(id)
 
-    async fetchUserInfo(id: number) {
-      const { data } = await getUserInfo<IResponseType<IUserInfo>>(id)
-      this.$state.userInfo = data
-      Cookies.set('userInfo', JSON.stringify(data))
-    },
+    router.push('/home')
+  }
 
-    async fetchMenu(id: number) {
-      const { data } = await getMenu(id)
-      this.$state.menus = data
-      Cookies.set('menus', JSON.stringify(data))
-      console.log(JSON.stringify(data))
-    },
+  function initStore() {
+    const token = Cookies.get('token')
+    const userInfo = Cookies.get('userInfo')
+    const menus = Cookies.get('menus')
 
-    // 用户刷新后，是否已登录是通过在storage或者cookie中存储的数据来进行判断的
-    // 但是因为用户刷新了界面，所以vuex或pinia中的状态会被全部置空
-    // 因此需要重新进行设置
-    initStore() {
-      const token = Cookies.get('token')
-      const userInfo = Cookies.get('userInfo')
-      const menus = Cookies.get('menus')
-
-      if (token && userInfo && menus) {
-        /* eslint-disable no-param-reassign */
-        this.$patch(state => {
-          state.token = token
-          state.userInfo = JSON.parse(userInfo ?? '{}')
-          state.menus = JSON.parse(menus ?? '{}')
-        })
-      }
+    if (token && userInfo && menus) {
+      tokenRef.value = token
+      userInfoRef.value = JSON.parse(userInfo ?? '{}')
+      menusRef.value = JSON.parse(menus ?? '{}')
     }
+  }
+
+  return {
+    tokenRef,
+    userInfoRef,
+    menusRef,
+    login,
+    fetchUserInfo,
+    fetchMenu,
+    initStore
   }
 })
